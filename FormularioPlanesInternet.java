451bc123382ffda29b3ce791d8b0989a767d35d6
97jessicavera <70911633+97jessicavera@users.noreply.github.com>
2020-09-22 21:40:09 -0500

    Add files via upload

---
+++ FormularioPlanesInternet.java
@@ -0,0 +1,376 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Vista;
+
+import com.toedter.calendar.JDateChooser;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author hp
+ */
+public class FormularioPlanesInternet extends javax.swing.JFrame {
+
+    /**
+     * Creates new form FormularioConstribuyente
+     */
+    public FormularioPlanesInternet() {
+        initComponents();
+    }
+
+    public JButton getFormularioPlanesIntenet_BntActualizar() {
+        return FormularioPlanesIntenet_BntActualizar;
+    }
+
+    public void setFormularioPlanesIntenet_BntActualizar(JButton FormularioPlanesIntenet_BntActualizar) {
+        this.FormularioPlanesIntenet_BntActualizar = FormularioPlanesIntenet_BntActualizar;
+    }
+
+    public JButton getFormularioPlanesIntenet_BntEditar() {
+        return FormularioPlanesIntenet_BntEditar;
+    }
+
+    public void setFormularioPlanesIntenet_BntEditar(JButton FormularioPlanesIntenet_BntEditar) {
+        this.FormularioPlanesIntenet_BntEditar = FormularioPlanesIntenet_BntEditar;
+    }
+
+    public JButton getFormularioPlanesIntenet_BntEliminar() {
+        return FormularioPlanesIntenet_BntEliminar;
+    }
+
+    public void setFormularioPlanesIntenet_BntEliminar(JButton FormularioPlanesIntenet_BntEliminar) {
+        this.FormularioPlanesIntenet_BntEliminar = FormularioPlanesIntenet_BntEliminar;
+    }
+
+    public JButton getFormularioPlanesIntenet_BntGuardar() {
+        return FormularioPlanesIntenet_BntGuardar;
+    }
+
+    public void setFormularioPlanesIntenet_BntGuardar(JButton FormularioPlanesIntenet_BntGuardar) {
+        this.FormularioPlanesIntenet_BntGuardar = FormularioPlanesIntenet_BntGuardar;
+    }
+
+    public JButton getFormularioPlanesIntenet_BontCancelar() {
+        return FormularioPlanesIntenet_BontCancelar;
+    }
+
+    public void setFormularioPlanesIntenet_BontCancelar(JButton FormularioPlanesIntenet_BontCancelar) {
+        this.FormularioPlanesIntenet_BontCancelar = FormularioPlanesIntenet_BontCancelar;
+    }
+
+    public JDateChooser getFormularioPlanesIntenet_JDate_FechaCreacion() {
+        return FormularioPlanesIntenet_JDate_FechaCreacion;
+    }
+
+    public void setFormularioPlanesIntenet_JDate_FechaCreacion(JDateChooser FormularioPlanesIntenet_JDate_FechaCreacion) {
+        this.FormularioPlanesIntenet_JDate_FechaCreacion = FormularioPlanesIntenet_JDate_FechaCreacion;
+    }
+
+    public JTable getFormularioPlanesIntenet_TablaGeneral() {
+        return FormularioPlanesIntenet_TablaGeneral;
+    }
+
+    public void setFormularioPlanesIntenet_TablaGeneral(JTable FormularioPlanesIntenet_TablaGeneral) {
+        this.FormularioPlanesIntenet_TablaGeneral = FormularioPlanesIntenet_TablaGeneral;
+    }
+
+    public JTextField getFormularioPlanesIntenet_Txt_Nombre_Tecnico() {
+        return FormularioPlanesIntenet_Txt_Nombre_Tecnico;
+    }
+
+    public void setFormularioPlanesIntenet_Txt_Nombre_Tecnico(JTextField FormularioPlanesIntenet_Txt_Nombre_Tecnico) {
+        this.FormularioPlanesIntenet_Txt_Nombre_Tecnico = FormularioPlanesIntenet_Txt_Nombre_Tecnico;
+    }
+
+    public JTextField getFormularioPlanesIntenet_txt_IDPlna() {
+        return FormularioPlanesIntenet_txt_IDPlna;
+    }
+
+    public void setFormularioPlanesIntenet_txt_IDPlna(JTextField FormularioPlanesIntenet_txt_IDPlna) {
+        this.FormularioPlanesIntenet_txt_IDPlna = FormularioPlanesIntenet_txt_IDPlna;
+    }
+
+    public JTextField getFormularioPlanesIntenet_txt_NombrePlan() {
+        return FormularioPlanesIntenet_txt_NombrePlan;
+    }
+
+    public void setFormularioPlanesIntenet_txt_NombrePlan(JTextField FormularioPlanesIntenet_txt_NombrePlan) {
+        this.FormularioPlanesIntenet_txt_NombrePlan = FormularioPlanesIntenet_txt_NombrePlan;
+    }
+
+    public JTextField getFormularioPlanesIntenet_txt_Precio() {
+        return FormularioPlanesIntenet_txt_Precio;
+    }
+
+    public void setFormularioPlanesIntenet_txt_Precio(JTextField FormularioPlanesIntenet_txt_Precio) {
+        this.FormularioPlanesIntenet_txt_Precio = FormularioPlanesIntenet_txt_Precio;
+    }
+
+    public JTextField getFormularioPlanesIntenet_txt_Velocidad() {
+        return FormularioPlanesIntenet_txt_Velocidad;
+    }
+
+    public void setFormularioPlanesIntenet_txt_Velocidad(JTextField FormularioPlanesIntenet_txt_Velocidad) {
+        this.FormularioPlanesIntenet_txt_Velocidad = FormularioPlanesIntenet_txt_Velocidad;
+    }
+
+    public JTextField getFormularioPlanesIntenet_txt_comparticionCanal() {
+        return FormularioPlanesIntenet_txt_comparticionCanal;
+    }
+
+    public void setFormularioPlanesIntenet_txt_comparticionCanal(JTextField FormularioPlanesIntenet_txt_comparticionCanal) {
+        this.FormularioPlanesIntenet_txt_comparticionCanal = FormularioPlanesIntenet_txt_comparticionCanal;
+    }
+
+    public JTextField getFormularioPlanesIntenet_txt_tipoCliente() {
+        return FormularioPlanesIntenet_txt_tipoCliente;
+    }
+
+    public void setFormularioPlanesIntenet_txt_tipoCliente(JTextField FormularioPlanesIntenet_txt_tipoCliente) {
+        this.FormularioPlanesIntenet_txt_tipoCliente = FormularioPlanesIntenet_txt_tipoCliente;
+    }
+    
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        FormularioPlanesIntenet_txt_IDPlna = new javax.swing.JTextField();
+        FormularioPlanesIntenet_txt_NombrePlan = new javax.swing.JTextField();
+        FormularioPlanesIntenet_Txt_Nombre_Tecnico = new javax.swing.JTextField();
+        FormularioPlanesIntenet_JDate_FechaCreacion = new com.toedter.calendar.JDateChooser();
+        FormularioPlanesIntenet_txt_comparticionCanal = new javax.swing.JTextField();
+        FormularioPlanesIntenet_txt_tipoCliente = new javax.swing.JTextField();
+        FormularioPlanesIntenet_txt_Velocidad = new javax.swing.JTextField();
+        FormularioPlanesIntenet_txt_Precio = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
+        FormularioPlanesIntenet_BntGuardar = new javax.swing.JButton();
+        FormularioPlanesIntenet_BontCancelar = new javax.swing.JButton();
+        FormularioPlanesIntenet_BntEditar = new javax.swing.JButton();
+        FormularioPlanesIntenet_BntActualizar = new javax.swing.JButton();
+        FormularioPlanesIntenet_BntEliminar = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel10 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        FormularioPlanesIntenet_TablaGeneral = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+        jPanel1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setText("ID Plan:");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(78, 61, -1, -1));
+
+        jLabel2.setText("Nombre Plan:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(52, 99, -1, -1));
+
+        jLabel3.setText("Nombre Tecnico:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(308, 61, -1, -1));
+
+        jLabel4.setText("Fecha Creacion:");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 100, -1, -1));
+
+        jLabel5.setText("Comparticion Canal:");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, -1, -1));
+
+        jLabel6.setText("Tipo Cliente:");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 190, -1, -1));
+
+        jLabel7.setText("Velocidad:");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 140, -1, -1));
+
+        jLabel8.setText("Precio:");
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 180, -1, -1));
+        jPanel1.add(FormularioPlanesIntenet_txt_IDPlna, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 58, 140, -1));
+        jPanel1.add(FormularioPlanesIntenet_txt_NombrePlan, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 96, 140, -1));
+        jPanel1.add(FormularioPlanesIntenet_Txt_Nombre_Tecnico, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 60, 140, -1));
+        jPanel1.add(FormularioPlanesIntenet_JDate_FechaCreacion, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 100, 130, -1));
+        jPanel1.add(FormularioPlanesIntenet_txt_comparticionCanal, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 140, 140, -1));
+        jPanel1.add(FormularioPlanesIntenet_txt_tipoCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 180, 140, -1));
+        jPanel1.add(FormularioPlanesIntenet_txt_Velocidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 140, 140, -1));
+        jPanel1.add(FormularioPlanesIntenet_txt_Precio, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 180, 140, -1));
+
+        jPanel2.setBackground(new java.awt.Color(0, 153, 0));
+
+        FormularioPlanesIntenet_BntGuardar.setText("Guardar");
+
+        FormularioPlanesIntenet_BontCancelar.setText("Cancelar");
+
+        FormularioPlanesIntenet_BntEditar.setText("Editar");
+
+        FormularioPlanesIntenet_BntActualizar.setText("Actualizar");
+
+        FormularioPlanesIntenet_BntEliminar.setText("Eliminar");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(FormularioPlanesIntenet_BntGuardar)
+                .addGap(18, 18, 18)
+                .addComponent(FormularioPlanesIntenet_BntEditar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(FormularioPlanesIntenet_BntActualizar)
+                .addGap(14, 14, 14)
+                .addComponent(FormularioPlanesIntenet_BntEliminar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(FormularioPlanesIntenet_BontCancelar)
+                .addContainerGap(414, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(FormularioPlanesIntenet_BntGuardar)
+                    .addComponent(FormularioPlanesIntenet_BontCancelar)
+                    .addComponent(FormularioPlanesIntenet_BntEditar)
+                    .addComponent(FormularioPlanesIntenet_BntActualizar)
+                    .addComponent(FormularioPlanesIntenet_BntEliminar))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 230, 850, 40));
+
+        jPanel3.setBackground(new java.awt.Color(0, 153, 0));
+
+        jLabel10.setText("Formuario de Planes de Internet");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(309, 309, 309)
+                .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(350, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(15, Short.MAX_VALUE)
+                .addComponent(jLabel10)
+                .addContainerGap())
+        );
+
+        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 850, 40));
+
+        FormularioPlanesIntenet_TablaGeneral.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ID Plan:", "Nombre Plan:", "Comparticion Canal:", "Tipo Cliente:", "Nombre Tecnico:", "Fecha Creacion:", "Velocidad:", "Precio:"
+            }
+        ));
+        jScrollPane1.setViewportView(FormularioPlanesIntenet_TablaGeneral);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 290, 820, 200));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 852, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 536, Short.MAX_VALUE)
+        );
+
+        jPanel1.getAccessibleContext().setAccessibleName("Formulario Contribuyente");
+        jPanel1.getAccessibleContext().setAccessibleParent(jPanel1);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FormularioPlanesInternet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FormularioPlanesInternet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FormularioPlanesInternet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FormularioPlanesInternet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FormularioPlanesInternet().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton FormularioPlanesIntenet_BntActualizar;
+    private javax.swing.JButton FormularioPlanesIntenet_BntEditar;
+    private javax.swing.JButton FormularioPlanesIntenet_BntEliminar;
+    private javax.swing.JButton FormularioPlanesIntenet_BntGuardar;
+    private javax.swing.JButton FormularioPlanesIntenet_BontCancelar;
+    private com.toedter.calendar.JDateChooser FormularioPlanesIntenet_JDate_FechaCreacion;
+    private javax.swing.JTable FormularioPlanesIntenet_TablaGeneral;
+    private javax.swing.JTextField FormularioPlanesIntenet_Txt_Nombre_Tecnico;
+    private javax.swing.JTextField FormularioPlanesIntenet_txt_IDPlna;
+    private javax.swing.JTextField FormularioPlanesIntenet_txt_NombrePlan;
+    private javax.swing.JTextField FormularioPlanesIntenet_txt_Precio;
+    private javax.swing.JTextField FormularioPlanesIntenet_txt_Velocidad;
+    private javax.swing.JTextField FormularioPlanesIntenet_txt_comparticionCanal;
+    private javax.swing.JTextField FormularioPlanesIntenet_txt_tipoCliente;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+}
